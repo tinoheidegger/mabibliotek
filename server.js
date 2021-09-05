62f9835337909ac290eb425526733c01840e3bf0
tinoheidegger <tinoheidegger83@gmail.com>
2021-09-05 00:38:45 +0000

    Initial Setup

---
+++ server.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+
+//Middleware
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(express.json());
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+
+//connect to mongodb
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+
+app.listen(process.env.PORT || 3000);
