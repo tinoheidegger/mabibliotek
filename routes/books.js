6d7175202d55aa1e2eaa6fe01bf70bd8766ddd16
tinoheidegger <84242169+tinoheidegger@users.noreply.github.com>
2021-09-08 03:14:22 +0000

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -23,10 +23,10 @@ router.get("/", async (req, res) => {
     query = query.regex("title", new RegExp(req.query.title, "i"));
   }
   if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
-    query = query.lte("publishedDate", req.query.publishedBefore);
+    query = query.lte("publishDate", req.query.publishedBefore);
   }
   if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
-    query = query.gte("publishedDate", req.query.publishedAfter);
+    query = query.gte("publishDate", req.query.publishedAfter);
   }
   try {
     const books = await query.exec();
